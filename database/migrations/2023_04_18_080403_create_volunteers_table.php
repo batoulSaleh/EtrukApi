d61e525fdd7b5431e0662e6743587cbcfb76318b
batoul saleh <batoulsaleh315@gmail.com>
2023-04-18 01:07:31 -0800

    volunteer3 commit

--- database/migrations/2023_04_18_080403_create_volunteers_table.php
+++ database/migrations/2023_04_18_080403_create_volunteers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone')->nullable();
             $table->string('city');
             $table->string('address')->nullable();

8ca87252c365b3fef3b3315559a88c3ae56b18b5
batoul saleh <batoulsaleh315@gmail.com>
2023-05-04 18:39:39 -0800

    admin accept donation2

--- app/Http/Controllers/api/user/UsDonationController.php
+++ app/Http/Controllers/api/user/UsDonationController.php
@@ -491,7 +491,7 @@ class UsDonationController extends Controller
 
         foreach($items as $item){
             $it=Item::find($item['id']);
-            if($item['amount']>$it->amount){
+            if((double)$item['amount']>(double)$it->amount){
                 $response = [
                     'message'=>'the'.$it->name_en .'amount must be less than or equal' . $it->amount,
                 ];

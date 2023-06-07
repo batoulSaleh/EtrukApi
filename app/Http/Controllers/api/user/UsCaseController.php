500cd6d23933ffda39bd941fc200c693553b3097
batoul saleh <batoulsaleh315@gmail.com>
2023-06-06 17:50:28 -0800

    bbb

--- app/Http/Controllers/api/user/UsCaseController.php
+++ app/Http/Controllers/api/user/UsCaseController.php
@@ -298,251 +298,47 @@ class UsCaseController extends Controller
                     'status.in'=> trans('api.notallowed'),
                 ]);
             }
-
             $request->validate([
-                'donationtype_id' =>'required|exists:donationtypes,id',
-            ],[
-                'donationtype_id.required'=> trans('api.required'),
-                'donationtype_id.exists'=> trans('api.exists'),
+                'description_en' => 'string|max:500',
+                'description_ar' => 'string|max:500',
+                'file' => 'file|max:2048',
             ]);
-
-            $items=Item::where('casee_id',$casee->id)->get();
-            if($items){
-                foreach($items as $item){
-                $item->delete();
-                }
-            }
-
-            if($casee->donationtype_id==4){
-                $casee->update([
-                    'type_en' => null,
-                    'type_ar' => null,
-                    'gender_en' => null,
-                    'gender_ar' => null,
-                ]);
-            }
-
-            if($request->donationtype_id==5){
-                $request->validate([
-                    'name_en' => 'required|string|max:200',
-                    'name_ar' => 'required|string|max:200',
-                    'description_en' => 'string|max:500',
-                    'description_ar' => 'string|max:500',
-                    'file' => 'file|max:2048',
-                    'category_id' =>'required|exists:categories,id',
-                    'items'=>'required'
-                ],[
-                    'name_en.required'=> trans('api.required'),
-                    'name_ar.required'=> trans('api.required'),
-                    'category_id.required'=> trans('api.required'),
-                    'category_id.exists'=> trans('api.exists'),
-                    'items.required'=> trans('api.required'),
-                ]);
     
-                if($request->file('file')){
-                    $file_path = $request->file('file')->store('api/casees','public');
-                    $file=asset('storage/'.$file_path);
-                }else{
-                    $file=$casee->file;
-                }
-
-                $initial_amount=0;
-                $items=$request->items;
-    
-                $casee->update([
-                        'name_en' => $request->name_en,
-                        'name_ar'=> $request->name_ar,
-                        'description_en'=> $request->description_en,
-                        'description_ar'=> $request->description_ar,
-                        'file' => $file,
-                        'donationtype_id'=> $request->donationtype_id,
-                        'category_id'=> $request->category_id,
-                        'initial_amount'=>$initial_amount,
-                        'remaining_amount'=>$initial_amount,
-                        'status'=>$request->status,
-                        'user_id'=>$request->user()->id,
-                    ]);
-    
-                    $images=$request->file('images');
-            if($images){
-                $old_images=Caseimage::where('casee_id',$casee->id)->get();
-                foreach($old_images as $old_image){
-                    $old_image->delete();
-                }                
-                    foreach($images as $image){
-                        $image_path = $image->store('api/casees','public');
-                        $image=asset('storage/'.$image_path);
-                    
-
-                        Caseimage::create([
-                            'casee_id'=>$casee->id,
-                            'image'=>$image
-                        ]);
-                    }
-            }
-
-                foreach($items as $item){
-                    Item::create([
-                        'name_ar'=>$item['name_ar'],
-                        'name_en'=>$item['name_en'],
-                        'amount'=>$item['amount'],
-                        'casee_id'=>$casee->id,
-                    ]);
-        
-                    $initial_amount=$initial_amount +$item['amount'];
-        
-                }
-            
-                $casee->update([
-                    'initial_amount'=>$initial_amount,
-                    'remaining_amount'=>$initial_amount,
-                ]);
-        
-                $final_items=Item::where('casee_id',$casee->id)->get();
-        
-                $response = [
-                    'message'=>trans('api.updated'),
-                    'case' => $casee,
-                    'items'=>$final_items,
-                ];
-            }elseif($request->donationtype_id==4){
-                $request->validate([
-                    'name_en' => 'required|string|max:200',
-                    'name_ar' => 'required|string|max:200',
-                    'description_en' => 'required|string|max:500',
-                    'description_ar' => 'required|string|max:500',
-                    'type_en' => 'required|string|max:500',
-                    'type_ar' => 'required|string|max:500',
-                    'gender_en' => 'required|string|max:500',
-                    'gender_ar' => 'required|string|max:500',
-                    'file' => 'file|max:2048',
-                    'category_id' =>'required|exists:categories,id',
-                    'initial_amount'=>'required|numeric',
-                ],[
-                    'name_en.required'=> trans('api.required'),
-                    'name_ar.required'=> trans('api.required'),
-                    'description_en.required'=> trans('api.required'),
-                    'description_ar.required'=> trans('api.required'),
-                    'type_en.required'=> trans('api.required'),
-                    'type_ar.required'=> trans('api.required'),
-                    'gender_en.required'=> trans('api.required'),
-                    'gender_ar.required'=> trans('api.required'),
-                    'category_id.required'=> trans('api.required'),
-                    'category_id.exists'=> trans('api.exists'),
-                    'initial_amount.required'=> trans('api.required'),
-                ]);
-    
-                if($request->file('file')){
-                    $file_path = $request->file('file')->store('api/casees','public');
-                    $file=asset('storage/'.$file_path);
-                }else{
-                    $file=$casee->file;
-                }
-    
-                $casee->update([
-                    'name_en' => $request->name_en,
-                    'name_ar'=> $request->name_ar,
-                    'description_en'=> $request->description_en,
-                    'description_ar'=> $request->description_ar,
-                    'type_en' => $request->type_en,
-                    'type_ar' => $request->type_ar,
-                    'gender_en' => $request->gender_en,
-                    'gender_ar' => $request->gender_ar,
-                    'file' => $file,
-                    'donationtype_id'=> $request->donationtype_id,
-                    'category_id'=> $request->category_id,
-                    'initial_amount'=>$request->initial_amount,
-                    'remaining_amount'=>$request->initial_amount,
-                    'status'=>$request->status,
-                    'user_id'=>$request->user()->id,
-                ]);
-
-                $images=$request->file('images');
-            if($images){
-                $old_images=Caseimage::where('casee_id',$casee->id)->get();
-                foreach($old_images as $old_image){
-                    $old_image->delete();
-                }                
-                    foreach($images as $image){
-                        $image_path = $image->store('api/casees','public');
-                        $image=asset('storage/'.$image_path);
-                    
-
-                        Caseimage::create([
-                            'casee_id'=>$casee->id,
-                            'image'=>$image
-                        ]);
-                    }
-            }
-
-                $response = [
-                    'message'=>trans('api.updated'),
-                    'case' => $casee
-                ];
-
+            if($request->file('file')){
+                $file_path = $request->file('file')->store('api/casees','public');
+                $file=asset('storage/'.$file_path);
             }else{
-                $request->validate([
-                    'name_en' => 'required|string|max:200',
-                    'name_ar' => 'required|string|max:200',
-                    'description_en' => 'string|max:500',
-                    'description_ar' => 'string|max:500',
-                    'file' => 'file|max:2048',
-                    'category_id' =>'required|exists:categories,id',
-                    'initial_amount'=>'required|numeric',
-                ],[
-                    'name_en.required'=> trans('api.required'),
-                    'name_ar.required'=> trans('api.required'),
-                    'category_id.required'=> trans('api.required'),
-                    'category_id.exists'=> trans('api.exists'),
-                    'initial_amount.required'=> trans('api.required'),
-                ]);
-        
-                if($request->file('file')){
-                    $file_path = $request->file('file')->store('api/casees','public');
-                    $file=asset('storage/'.$file_path);
-                }else{
-                    $file=$casee->file;
-                }
+                $file=$casee->file;
+            }
 
-                $casee->update([
-                    'name_en' => $request->name_en,
-                    'name_ar'=> $request->name_ar,
+            $casee->update([
                     'description_en'=> $request->description_en,
                     'description_ar'=> $request->description_ar,
                     'file' => $file,
-                    'donationtype_id'=> $request->donationtype_id,
-                    'category_id'=> $request->category_id,
-                    'initial_amount'=>$request->initial_amount,
-                    'remaining_amount'=>$request->initial_amount,
                     'status'=>$request->status,
                     'user_id'=>$request->user()->id,
                 ]);
-
-                $images=$request->file('images');
+    
+            $images=$request->file('images');
             if($images){
                 $old_images=Caseimage::where('casee_id',$casee->id)->get();
                 foreach($old_images as $old_image){
                     $old_image->delete();
                 }                
-                    foreach($images as $image){
-                        $image_path = $image->store('api/casees','public');
-                        $image=asset('storage/'.$image_path);
-                    
-
-                        Caseimage::create([
-                            'casee_id'=>$casee->id,
-                            'image'=>$image
-                        ]);
-                    }
+                foreach($images as $image){
+                    $image_path = $image->store('api/casees','public');
+                    $image=asset('storage/'.$image_path);
+                    Caseimage::create([
+                        'casee_id'=>$casee->id,
+                        'image'=>$image
+                    ]);
+                }
             }
-                
-                $response = [
-                    'message'=>trans('api.updated'),
-                    'case' => $casee
-                ];
-            }
-        }
-        else{
+            $response = [
+                'message'=>trans('api.updated'),
+                'case' => $casee,
+            ];
+        }else{
             $response = [
                 'message'=>trans('api.notallowed'),
             ];}
